7fcd0cdda98e94bea882814e75d8185d2f44e12c
adrix <adrian.gonser@icloud.com>
2024-11-13 19:38:33 +0100

    Router test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         {/* Navigation */}
         <nav>
