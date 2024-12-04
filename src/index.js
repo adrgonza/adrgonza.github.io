81063563550d875c5719906e8ac5776d3b835a4e
adrix <adrian.gonser@icloud.com>
2024-12-04 10:47:59 +0100

    Fixed refreshing page and getting 404

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js (App.js)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
@@ -11,9 +11,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        {/* Navbar */}
         <Navbar />
-        {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
