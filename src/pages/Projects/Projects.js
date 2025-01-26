8b19c0bd3573ec52ae5ee65bbf32661855a3b870
adrix <adrian.gonser@icloud.com>
2025-01-26 13:25:24 +0100

    performance improved 200%

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -107,7 +107,7 @@ const Projects = () => {
               <strong>Pixel Pong</strong> is a multiplayer take on the classic Pong 
               game, enhanced with delightful pixel art.
             </p>
-            <video controls src="https://media-hosting.imagekit.io//5bb670818cc24fd4/377062596-09fb5cc9-d574-46b7-9f25-6e012b8fe88e.mov?Expires=1832350371&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Z8b3k6xRE4SzwvYnudIcdClgQz5G4kbafHnZhvtuDWZOaBJbFdg3YM9OrZTdu6Zie1apz893cs2S~~KdDSsf8jEkhO6D-hkOsPVhQSI8zDZ66IkCbfs-aEeesuTtHyNxnd0CWCjDdbLH-q1y0MdWraqnKatTk7qaDcBiHk-io2fmuUv97ajjUJkwbbh00hN9gRw-WR1clER3UTLJGQRVBKUWbqfqpCYgJw16wWbpxfrbWk8gd8xgGLx1wCzUTOkuO9BxQzi7s0NMyRaSvEJJEh58~W9bQcGsQ2~8Zi~VqPRX1kMaUXnkapRxdbM0ZiWYN-LwaisayfR5EF7D6zr46g__" className="project-video"></video>
+            <video controls preload="none" src="https://media-hosting.imagekit.io//0a903c845c3a46db/377062596-09fb5cc9-d574-46b7-9f25-6e012b8fe88e.mp4?Expires=1832500648&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=l6qzL-ApNqNCUH1wQGjUcBo6MI8VADHoIBxvacz2ZQ2pE1a5X-MOkpHzv6~jxuAb4xyM~V9GCmvE5K6Usesazm-d6w1gaWWyBa6ZgpBiYDA-tWVac3QutRbJAtkKkElgrZuVCHfjGLUxTdgEEr3rE~teBa1-Olf3Q-2fDSowGQ3HYu4WHPOsNHP7A9y6jvyotzNo6-49-VNcugUumGYm-gM6h1c4qGxYsslV-lvrj7wt1-w4VdryGh0vDYtxChG2Gmm5wKwMsIFpQVRGWH3At8PwSgLzZPx5vS5auJsm9s2WyMWEadWNVNWI6hjrsuVbRxn13N1FIjBKVRUZp4prBw__" className="project-video"></video>
             <p>
               This was the final project in the 42 common core, where I led the creative 
               design and front-end implementation.
@@ -127,7 +127,7 @@ const Projects = () => {
               <strong>Matcha</strong> is a modern dating app that connects people based 
               on real-time location and shared interests.
             </p>
-            <video controls src="https://media-hosting.imagekit.io//3dda1f6081ad4fb0/Screen%20Recording%202025-01-24%20at%2021.16.45.mov?Expires=1832357878&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=O-6GPSO92HH5vbe9QMHABcaMFVLRQkBmRM32n6ai~ve5BgI~BOAAaIA3EXR-wJojkVVg4-uiDJm5bY6SVuuWI1dPvbPCS7kH0dkJFKHPOnfK~gKX7kEt7-K4cXVY7NOHn8nC6~RLLIBFvZxQ-Dz8jHAdMUNBkNIgjN6weF2StbIYyE-57dLrs5vXfum9GDFyxB~u~g4KlqvEDFikRgTz6L6vPK-CEiK3NBhft-4ZJZFqIE3k~8g4rkmFmyZC49MIrN-tF2rvfUltIqqt0CHEW-gm8MhNArv~NQ1dBabSxZbB0evxShIdXR-E2Go1Z9Qc6zmqOWOE7FqaGeZhNEpznA__" className="project-video"></video>
+            <video controls preload="none" src="https://media-hosting.imagekit.io//4654510d12e544d2/Screen%20Recording%202025-01-26%20at%2013.18.56.mp4?Expires=1832502198&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=3K1rL87qWxSxv9gfvcOVkU7JoiaJp61aE7jBU~njSQtm2cb0HYhmDkVRPi02c4wb1xvgFCK5W7RPHNkQKcHAlb4d9YfDvNsHC~Ga04Yv1t9VHj~yciibw3eddzQbqYBPPacaT2n-THHQYh6SiyiWgyFXazRAeFSzVI8l-dzSbidlk-ezdw2naLr-aUcK3AAtF06YH~-sVzP65HStduZv~HYa2cdzhcPqdwnsTkSvUxNilzHgYcZFm9ZvghApS2gujqxkaFp--bDdcpX4DthrCa2Xb4gw4LIezuNZR25OoORwt~ITdA7yy-a46itZtS6CRLIkflrYi4uE61J2oNlolQ__" className="project-video"></video>
             <p>
               Built with <strong>React</strong>, <strong>Express</strong>, and <strong>PostgreSQL</strong>, it offers robust
               privacy settings and a clean, intuitive interface for users seeking meaningful connections.
@@ -147,7 +147,7 @@ const Projects = () => {
               to create a containerized trading network. Each user runs in a separate container, defined by a single 
               configuration file.
             </p>
-            <video controls src="https://media-hosting.imagekit.io//9ae02b7be21d4f89/IMG_8706.MP4?Expires=1832356167&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=kEwdeTi1vxeRDtxVe-D6-z~CZZiMk6pievJDVFhoq3~dk~i1LwpzCi~ZacZ1~hZXGLsBb2k12SK-owqzHvC8zfbvPvEwgS7KiVqNeRuEO5fhhkURzZv7wWaWl8zJd2v-IHITcA63-rsLGJYKJ7vsfvVPLLcCRWjnhSdFhLKR-wFsPKtmyId4wuBvYHa-TnKTrXazd0BRMOpX~7ugcoW0dL8JdJ3BWfEW5Yj6f7c5APMz~Y4beGqJQX1CnnSjeEnGngRD8NGWHk7fOMuuMCqOUp9oDeOyVHpryggIWwSqKu4rmJLaS58k5nT2qLQ6zwA2Y7Sj9zl3vwKoBQOhM2-P1A__" className="project-video"></video>
+            <video controls preload="none" src="https://media-hosting.imagekit.io//dd4040bbd54b4da0/IMG_8706.mp4?Expires=1832500222&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=MBMC9zwjE39ohQq3ViUEv~fDSP67-4C1PcrMPyDhlsAiymi1wQEI8~~kiiSvP28oRUzMExSiKI2soUvkPPp0ADrRg05X7VEcGMnAqJnT-FtaH4xZj5zBp1lodxhm2VuO5LX5Rf06tDxWyAyeaB0~nVbrkA~YaccjzsQ5INfHp52fTPWbEgTUr1NEq6WUHqJFHuupTcGC-eRsO3LzRrD1E29VyAGx2jiuGqDzh1YfaD3L5K03F4DXqX95pNjIr3LPuV1x~wEMapYanvo9~DW5ey0MXYO7d~C7RmuASL5JY7iSPJNSXNQRhYjpM9Bp057dQNOXN1eYFcm02nl2DnjYAQ__" className="project-video"></video>
             <p>
               A central server receives trading signals and distributes them to user containers, which then place 
               orders through multiple crypto exchange APIs. This approach ensures scalable and secure multi-user 
@@ -167,13 +167,13 @@ const Projects = () => {
               <strong>Midlands</strong> is a robust application that demonstrates 
               dynamic graphics rendering using C and specialized libraries.
             </p>
-            <img src="https://i.ibb.co/hZsGvpp/Screenshot-2025-01-24-at-19-22-08.png" alt="Project Screenshot" className="project-video" />
+            <img src="https://i.ibb.co/7JpDknn/Screenshot-2025-01-24-at-19-22-08.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
             <p>
               Additionally, there is also a way to edit maps and create your own levels.
               The map is simply an array of characters that you can modify at any time, 
               and the game will analyze this array to render the new layout in real-time.
             </p>
-            <img src="https://i.ibb.co/FD3ynDN/Screenshot-2025-01-24-at-20-55-30.png" alt="Project Screenshot" className="project-video" />
+            <img src="https://i.ibb.co/gjyrNF3/Screenshot-2025-01-24-at-20-55-30.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
             <a href="https://github.com/adrgonza/so_long" className="project-link">GitHub</a>
           </div>
         </div>
@@ -189,12 +189,12 @@ const Projects = () => {
               <strong>MineWorld</strong> is a 2D/3D hybrid game inspired by 
               sandbox-style exploration and raycasting techniques.
             </p>
-            <video controls src="https://media-hosting.imagekit.io//5b287196b4cd433f/Screen%20Recording%202025-01-24%20at%2019.33.46%202.mov?Expires=1832355083&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=o90FJgvldCV-2nR8dIZMT2UxKVejAWHN0wUnStkKLmMN85-Ti4TKjH2EughtFNplTTOTRYFsHLMVcOWy6NU4jdyujIQngnvB2SuAW3tynLIlEbY7D9~h-i4~vxf7GYOvl~cW~yxL7U8nUzDNjadc7omPGpMAOZRzvIcoldTvbDokov-NI0Nvv~-e0NWkuMuGE5U0o8uFsD~SHyY1udmuFMFHMpX6O7~c7h49pVmqUxUcytou~rXi3c6DEe0uEnkvVPtI7F~yv~~OkAKNmOM5xCvJd9r0QjtDWEye7YvbMEvJDb6bXORMiUvkxbCoH7l9t99sVtEeLEoCGyLA1RIE-A__" className="project-video"></video>
+            <video controls preload="none" src="https://media-hosting.imagekit.io//0cb89fd3d10344f4/Screen%20Recording%202025-01-24%20at%2019.33.46%202.mp4?Expires=1832499867&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Sl94n~nITf6YrAGBrgd~q5YwWTtapni7BdtKlzd32AiYLMnA7BwEoCDXT1Yz6sEm6soOVHyB8EDWhloPFeEGEcxLGiCBRflE7VcN8as1gLB2HoPUghSwIXDSWrGP0rAwaov-ay9vrwNIom1J1jjXHwmx9iw9AJ3IrbSRCvZOs8E4auc3TpOKPOBLXvgX4Qn9X4OmbouQD9w2P5zovy~cwXOJocJwgVLdQWGk3Ii7nSp~AekdzhZpdxwlVX4jijD5Xs7elAPDizvYA9CwExSYCXaHNdUS2aj4-vHcZKTT97CWP~0qRG0JuOF~X22qamOgHeEFR8rxJcQUp58ute~dtw__" className="project-video"></video>
             <p>
               Developed with <strong>C</strong>, <strong>Graphic libraries</strong>, and <strong>Raycasting </strong> 
               to create procedurally generated maps and immersive environments.
             </p>
-            <img src="https://i.ibb.co/9bcFRsF/Screenshot-2025-01-24-at-19-31-53.png" alt="Project Screenshot" className="project-video" />
+            <img src="https://i.ibb.co/7SywnHW/Screenshot-2025-01-24-at-19-31-53.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
             <a href="https://github.com/adrgonza/cub3D" className="project-link">GitHub</a>
           </div>
         </div>
@@ -211,7 +211,7 @@ const Projects = () => {
               to <strong>Bash</strong> with extensive features, handling core command 
               functionalities and scripting basics.
             </p>
-            <video controls src="https://media-hosting.imagekit.io//b017fba8ecc24c13/Screen%20Recording%202025-01-24%20at%2021.07.00.mov?Expires=1832357329&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=mIM4CVKZ3ewLgAM4XXnb4Tdg7KHxtFNFPbaW7xZBASEtAJgNHW2-LoSUHPOLMmIXEWRZ-z3BDQsqQIJ6zlRzljcVe1bSdNdhw4f7y61PV22R76y6vxqli11GF2vsFU6kgAidaRfNZosxIw0hq7-Z3x08VHmfpbcLxSXH6erReH2KQ21akbS5nM~0C-D53lZXISVxe~pofhdfDoCCgyBs7SCpguztyr2-FCk4ckiR5VPpX13xlNtQiZSCFl~hCI4-8nJbhnywK~OKMOfBJc~zCjoGvXf2BeugQEvyLrfSvcryTZr2AQbvOxZPekK3pBHwjdDrlHQlVFB0hwuXtQDmjA__" className="project-video"></video>
+            <video controls preload="none" src="https://media-hosting.imagekit.io//a3d3054fd96945f6/Screen%20Recording%202025-01-24%20at%2021.07.00.mp4?Expires=1832501645&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=p9quDfVLuiFXDUFjErevg8pLXBcHiN2De6HzjDL9d8t8fCBbxlYapGrOYBWc3ClHyC0zC1vDP7J6XDDJfSI4rXjpdudO6pbSHgvmxLpkASvLtTB59vzZW5clXB7JuTeNYKBkaU4uTGMKFdyf7pkk9GwIEoKcM1jse8GPqx3syY9kWj7e7O0Lh6ZoIncJTjK~dJaoYFMkssDoSzBrJndfZ4~9Tjf~6J~AFJsHnnDFWRi7xvHPe3rSsLrtcUFFxtnE6WDl2~MQSZxwRBkeJiEpJ0UjNc0fgo4b6Vlcjl4~0HiPOe1XDo3kg4xCqcsHg9UDSjia08JQV6qhoPhFCIydbg__" className="project-video"></video>
             <p>
               Written in <strong>C</strong> and inspired by <strong>Bash</strong>, it interprets 
               user commands, manages processes, manipulates files, and supports a wide range 
@@ -232,7 +232,7 @@ const Projects = () => {
               <strong>IRC</strong> is a classic chat server implementation in C++, 
               allowing real-time messaging and channel creation.
             </p>
-            <img src="https://www.illumy.com/wp-content/uploads/2023/08/WeeChat_3.4_screenshot11.png" alt="Project Screenshot" className="project-video" />
+            <img src="https://i.ibb.co/fk0hRP5/Wee-Chat-3.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
             <p>
               Written in <strong>C++</strong> and modeled after Internet Relay Chat 
               protocols for multi-user text communication.
@@ -272,15 +272,15 @@ const Projects = () => {
               the essence of adolescence. Every image was shot, developed, and scanned 
               by me at home, capturing genuine, unfiltered moments.
             </p>
-            <img src="https://i.ibb.co/hfhnFfP/MG-8841-CR2.jpg" alt="Project Screenshot" className="project-video" />
+            <img src="https://i.ibb.co/Z6DXWgs/MG-8841-CR2.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
             <p>
               Using analog techniques, <strong>Youthbook</strong> immerses readers in the 
               raw energy and delicate grain of film, reflecting the excitement, 
               vulnerability, and fleeting nature of youth.
             </p>
-            <img src="https://i.ibb.co/JdrbSHt/50872-D97-5-B5-F-461-C-BEB7-6-B95-F1-AFAAEC.jpg" alt="Project Screenshot" className="project-video" />
-            <img src="https://i.ibb.co/zXrKdt1/IMG-3288.jpg" alt="Project Screenshot" className="project-video" />
-            <img src="https://i.ibb.co/mDjX1Lp/C724809-E-207-A-4182-9-B05-3-A7-F3-DAB9-EA1.jpg" alt="Project Screenshot" className="project-video" />
+            <img src="https://i.ibb.co/W5LxXZQ/50872-D97-5-B5-F-461-C-BEB7-6-B95-F1-AFAAEC.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
+            <img src="https://i.ibb.co/Z1gDZ0s/IMG-3288.webp" alt="Project Screenshot" className="project-video" loading="lazy"/>
+            <img src="https://i.ibb.co/mDjX1Lp/C724809-E-207-A-4182-9-B05-3-A7-F3-DAB9-EA1.jpg" alt="Project Screenshot" className="project-video" loading="lazy"/>
 
           </div>
         </div>
